1f068f4eddb09a8e4fdf041827bc5762a706ea46
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-05 03:29:38 -0700

    prints followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ * return - always zero
+ */
+
+int main(void)
+{
+	putchar("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

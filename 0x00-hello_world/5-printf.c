4a1ccaafef9b33d653f01e6bccd8048130f10683
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-05 03:44:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* main - write characters followed by a new line
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

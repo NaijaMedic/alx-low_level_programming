be196e76bbcc94782b0f167e0a5366b42ee939e4
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-07 00:17:47 -0700

    numbers with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int c;
 
-	for (c = 0; c < 10; c++)
+	for (c = 0; c <= 9; c++)
 	{
 		putchar('0' + c);
 		if (c != 9)
@@ -17,5 +17,6 @@ int main(void)
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }

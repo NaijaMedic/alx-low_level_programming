533286c4d512da5b75f206046a0e986a2dcf6d1d
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-06 23:50:34 -0700

    print with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing numbers followed by comma
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			putchar('0' + c);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}

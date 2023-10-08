bd274833e1086b3e5fa7a842a14fb8375f061eb3
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-08 06:49:34 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd>
 /**
-<<<<<<< HEAD
  * main - write code without printf and put command
-=======
- * main - prints without using printf or puts
->>>>>>> b3b4b4e827374db870130c95eedee13cd63de4f2
  * Return: 0
  */
 
 int main(void)
 {
-<<<<<<< HEAD
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-=======
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n",
->>>>>>> b3b4b4e827374db870130c95eedee13cd63de4f2
 	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }

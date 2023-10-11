2163480cb68cb6e7aa7d7978837d92e1de6cf6db
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-11 13:03:52 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,20 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
-		printf("\n");
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		_putchar('-');
 		return (-1);
-		printf("\n");
 	}
 	else
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
-		printf("\n");
 	}
 }

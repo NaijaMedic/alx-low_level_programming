ed85d4e9ab61c55c9f68222df108d4a37b92b382
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-12 09:24:29 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - prints uppercase letter
+ * @c: character to be printed
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+	return (0);
+}

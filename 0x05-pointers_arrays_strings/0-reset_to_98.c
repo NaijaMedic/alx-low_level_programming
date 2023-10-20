1259a9d4b386e61ed688843f91b6d437d4a60cb2
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-10-20 13:49:46 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes parameter
+ * n: parameter value
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int *n;
+	n = 98;
+}

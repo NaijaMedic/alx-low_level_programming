5258c69077b2eb3b679857e6673ec8b8a8b9c5fd
Adebiyi Nimotallahi <adebiyinimotallahi@gmail.com>
2023-11-18 19:38:29 -0800

    0-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to be printed
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar("Puts with recursion");
+	_putchar("\n");
+}
